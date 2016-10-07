fa440c9eff96ca56b924440fe654e900a8f77524
erick <apoloerick02@gmail.com>
2016-10-07 19:41:18 -0400

    hw

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'tBQ3PmZTmmbBlfTAgQ1LjwGJZ',
+  consumer_secret: 'XNlkCU04WK8FQKJpQKtwq1El1Pl29j8Lvs4Xy5DcBQyY6pkLO7',
+  access_token_key: '	87253205-wPITvZFzR1DrCFchYjINGOdxxN8Yx5aC9JESUXMzV',
+  access_token_secret: 'h1nazyoQBuU23m4V08XR790aAfMIRIGJXfP7DWISDnOn0',
+}
